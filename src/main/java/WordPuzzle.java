2ff5d138a48b6a61a59abb869e983fc4872c3513
PolinaNenchev <polina.mihova@yahoo.com>
2016-02-12 15:55:05 -0800

    Add functionality and user interface

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -18,21 +18,18 @@ public class WordPuzzle {
 
     get("/detector", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      String userString = request.queryParams("word");
-      String noVowelString = WordPuzzle.checkWord(userString);
+      model.put("template","templates/detector.vtl");
 
-      model.put("userString", userString);
-      model.put("noVowelString", noVowelString);
-      model.put("template", "templates/detector.vtl");
+      String userInputString = request.queryParams("word");
+      String results = WordPuzzle.checkWord(userInputString);
 
-      Object results = WordPuzzle.checkWord(userString);
       model.put("results", results);
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
 
-  public static String checkWord(String word) {
-    return word.replaceAll("[aeiouyAEIOUY]", "-");
+  public static String checkWord(String userInputString) {
+    return userInputString.replaceAll("[aeiouyAEIOUY]", "-");
 
   }
 }
