46ec06bb56e007beef89af336c829aeb8c976d3e
PolinaNenchev <polina.mihova@yahoo.com>
2016-02-12 13:49:30 -0800

    Create passing specs with check for words with no vowels and for words with vowels to be replaced with dash symbol

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -7,7 +7,7 @@ import static spark.Spark.*;
 
 public class WordPuzzle {
   public static void main(String[] args) {
-    staticFileLocation("/public");
+    // staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     //
